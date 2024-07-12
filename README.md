abc6db19d0ea4c8b558cf4afde1a55ce9595eca6
Otto Allmendinger <otto@bitgo.com>
2024-07-12 10:38:57 +0200

    Add initial version of wasm-miniscript package
    Issue: BTC-1317

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wasm-miniscript
-WebAssembly bindings for rust-miniscript
+
+This is a wrapper around the [rust-miniscript](https://github.com/rust-bitcoin/rust-miniscript) crate that is compiled
+to WebAssembly. It allows you to use Miniscript in NodeJS and in the browser.
+
+
+# Dependencies
+
+- [Rust](https://www.rust-lang.org/) nightly
+- [wasm-pack](https://rustwasm.github.io/wasm-pack/) (install with `cargo install wasm-pack`)
+- [NodeJS](https://nodejs.org/en/)
---
+++ packages/wasm-miniscript/js/index.ts
@@ -0,0 +1,36 @@
+import * as wasm from "./wasm/wasm_miniscript";
+
+type MiniscriptNode = unknown;
+
+type Miniscript = {
+  node(): MiniscriptNode;
+  toString(): string;
+  encode(): Uint8Array;
+};
+
+type ScriptContext = "tap" | "segwitv0" | "legacy";
+
+export function miniscriptFromString(
+  script: string,
+  scriptContext: ScriptContext,
+): Miniscript {
+  return wasm.miniscript_from_string(script, scriptContext);
+}
+
+export function miniscriptFromBitcoinScript(
+  script: Uint8Array,
+  scriptContext: ScriptContext,
+): Miniscript {
+  return wasm.miniscript_from_bitcoin_script(script, scriptContext);
+}
+
+type DescriptorNode = unknown;
+
+type Descriptor = {
+  node(): DescriptorNode;
+  toString(): string;
+};
+
+export function descriptorFromString(descriptor: string): Descriptor {
+  return wasm.descriptor_from_string(descriptor);
+}
