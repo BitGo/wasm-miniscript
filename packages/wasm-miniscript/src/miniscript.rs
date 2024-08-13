b545831f8cc701d83791a59333d7d2c12c0606da
Otto Allmendinger <otto@bitgo.com>
2024-08-13 17:22:00 +0200

    fix: use JsError instead of JsValue for errors
    It is the more appropriate type
    
    Issue: BTC-1348

--- packages/wasm-miniscript/src/miniscript.rs
+++ packages/wasm-miniscript/src/miniscript.rs
@@ -1,7 +1,6 @@
-use std::str::FromStr;
 use miniscript::bitcoin::{PublicKey, XOnlyPublicKey};
 use miniscript::{bitcoin, Legacy, Miniscript, Segwitv0, Tap};
-
+use std::str::FromStr;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::{JsError, JsValue};
 
@@ -70,21 +69,18 @@ impl From<Miniscript<PublicKey, Legacy>> for WrapMiniscript {
 }
 
 #[wasm_bindgen]
-pub fn miniscript_from_string(script: &str, context_type: &str) -> Result<WrapMiniscript, JsValue> {
+pub fn miniscript_from_string(script: &str, context_type: &str) -> Result<WrapMiniscript, JsError> {
     match context_type {
-        "tap" => Ok(WrapMiniscript::from(crate::error::wrap_err(Miniscript::<
-            XOnlyPublicKey,
-            Tap,
-        >::from_str(script))?)),
-        "segwitv0" => Ok(WrapMiniscript::from(crate::error::wrap_err(Miniscript::<
-            PublicKey,
-            Segwitv0,
-        >::from_str(script))?)),
-        "legacy" => Ok(WrapMiniscript::from(crate::error::wrap_err(Miniscript::<
-            PublicKey,
-            Legacy,
-        >::from_str(script))?)),
-        _ => Err(JsValue::from_str("Invalid context type")),
+        "tap" => Ok(WrapMiniscript::from(
+            Miniscript::<XOnlyPublicKey, Tap>::from_str(script).map_err(JsError::from)?,
+        )),
+        "segwitv0" => Ok(WrapMiniscript::from(
+            Miniscript::<PublicKey, Segwitv0>::from_str(script).map_err(JsError::from)?,
+        )),
+        "legacy" => Ok(WrapMiniscript::from(
+            Miniscript::<PublicKey, Legacy>::from_str(script).map_err(JsError::from)?,
+        )),
+        _ => Err(JsError::new("Invalid context type")),
     }
 }
 
@@ -92,28 +88,26 @@ pub fn miniscript_from_string(script: &str, context_type: &str) -> Result<WrapMi
 pub fn miniscript_from_bitcoin_script(
     script: &[u8],
     context_type: &str,
-) -> Result<WrapMiniscript, JsValue> {
+) -> Result<WrapMiniscript, JsError> {
     let script = bitcoin::Script::from_bytes(script);
     match context_type {
-        "tap" => Ok(WrapMiniscript::from(crate::error::wrap_err(Miniscript::<
-            XOnlyPublicKey,
-            Tap,
-        >::parse(script))?)),
-        "segwitv0" => Ok(WrapMiniscript::from(crate::error::wrap_err(Miniscript::<
-            PublicKey,
-            Segwitv0,
-        >::parse(script))?)),
-        "legacy" => Ok(WrapMiniscript::from(crate::error::wrap_err(Miniscript::<
-            PublicKey,
-            Legacy,
-        >::parse(script))?)),
-        _ => Err(JsValue::from_str("Invalid context type")),
+        "tap" => Ok(WrapMiniscript::from(
+            Miniscript::<XOnlyPublicKey, Tap>::parse(script).map_err(JsError::from)?,
+        )),
+        "segwitv0" => Ok(WrapMiniscript::from(
+            Miniscript::<PublicKey, Segwitv0>::parse(script).map_err(JsError::from)?,
+        )),
+        "legacy" => Ok(WrapMiniscript::from(
+            Miniscript::<PublicKey, Legacy>::parse(script).map_err(JsError::from)?,
+        )),
+        _ => Err(JsError::new("Invalid context type")),
     }
 }
 
-
 #[test]
 pub fn panic_xprv() {
+    use miniscript::bitcoin::secp256k1::Secp256k1;
+    use miniscript::Descriptor;
     let (d,m) = Descriptor::parse_descriptor(
         &Secp256k1::new(),
         "wsh(multi(2,xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U/2147483647'/0,xprv9vHkqa6EV4sPZHYqZznhT2NPtPCjKuDKGY38FBWLvgaDx45zo9WQRUT3dKYnjwih2yJD9mkrocEZXo1ex8G81dwSM1fwqWpWkeS3v86pgKt/1/2/*,xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi/10/20/30/40/*'))",
