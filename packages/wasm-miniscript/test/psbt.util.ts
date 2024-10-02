a6912d7f5071c378f43848f1eae30fa884aae153
Otto Allmendinger <otto@bitgo.com>
2024-10-02 13:42:23 +0200

    feat: add test for OP_DROP in miniscript
    Issue: BTC-1451

--- packages/wasm-miniscript/test/psbt.util.ts
+++ packages/wasm-miniscript/test/psbt.util.ts
@@ -1,8 +1,12 @@
 import * as utxolib from "@bitgo/utxo-lib";
 import { Descriptor, Psbt } from "../js";
 
-export function toWrappedPsbt(psbt: utxolib.bitgo.UtxoPsbt | Buffer | Uint8Array) {
-  if (psbt instanceof utxolib.bitgo.UtxoPsbt) {
+function toAddress(descriptor: Descriptor, network: utxolib.Network) {
+  utxolib.address.fromOutputScript(Buffer.from(descriptor.scriptPubkey()), network);
+}
+
+export function toWrappedPsbt(psbt: utxolib.bitgo.UtxoPsbt | utxolib.Psbt | Buffer | Uint8Array) {
+  if (psbt instanceof utxolib.bitgo.UtxoPsbt || psbt instanceof utxolib.Psbt) {
     psbt = psbt.toBuffer();
   }
   if (psbt instanceof Buffer || psbt instanceof Uint8Array) {
@@ -24,11 +28,20 @@ export function toUtxoPsbt(psbt: Psbt | Buffer | Uint8Array) {
 }
 
 export function updateInputWithDescriptor(
-  psbt: utxolib.bitgo.UtxoPsbt,
+  psbt: utxolib.Psbt,
   inputIndex: number,
   descriptor: Descriptor,
 ) {
   const wrappedPsbt = toWrappedPsbt(psbt);
   wrappedPsbt.updateInputWithDescriptor(inputIndex, descriptor);
   psbt.data.inputs[inputIndex] = toUtxoPsbt(wrappedPsbt).data.inputs[inputIndex];
+}
+
+export function finalizePsbt(psbt: utxolib.Psbt) {
+  const wrappedPsbt = toWrappedPsbt(psbt);
+  wrappedPsbt.finalize();
+  const unwrappedPsbt = toUtxoPsbt(wrappedPsbt);
+  for (let i = 0; i < psbt.data.inputs.length; i++) {
+    psbt.data.inputs[i] = unwrappedPsbt.data.inputs[i];
+  }
 }
